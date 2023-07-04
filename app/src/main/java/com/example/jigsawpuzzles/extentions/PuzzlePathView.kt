1e9626a9adda1f523ba3db35ec612f7bb31be490
rva2007 <rva2007@yandex.com>
2023-07-04 21:26:48 +0700

    class PuzzlePathView.kt was refactored

--- app/src/main/java/com/example/jigsawpuzzles/extentions/PuzzlePathView.kt
+++ app/src/main/java/com/example/jigsawpuzzles/extentions/PuzzlePathView.kt
@@ -15,18 +15,15 @@ class PuzzlePathView @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : AppCompatImageView(context, attrs, defStyleAttr) {
 
-    private var columnsNumberByDefault: Int = 4
-    var num: Int = columnsNumberByDefault
+    private var numberByDefault: Int = 4
+    var num: Int = numberByDefault
     private var paint: Paint = Paint().apply {
         style = Paint.Style.STROKE
         color = resources.getColor(R.color.brown, null)
         strokeWidth = 4f
         isAntiAlias = true
-
     }
-
     private var path: Path = Path()
-
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
@@ -34,125 +31,187 @@ class PuzzlePathView @JvmOverloads constructor(
         canvas?.drawPath(path, paint)
     }
 
-
     private fun preparePath(num: Int): Path {
         val columns: Int?
         val rows: Int?
+        val bigSideOfPuzzlePathView = 4
+        val smallSideOfPuzzlePathView = 3
+        val pathOfView = Path()
 
         if (screenOrientationIsPortrait()) {
             columns = num
-            rows = columns * 4 / 3
+            rows = (columns * bigSideOfPuzzlePathView) / smallSideOfPuzzlePathView
         } else {
             rows = num
-            columns = rows * 4 / 3
+            columns = (rows * bigSideOfPuzzlePathView) / smallSideOfPuzzlePathView
         }
-
-        val path1 = Path()
 
         //calculate the width and the height of the pieces
         val pieceWidth = measuredWidth / columns
         val pieceHeight = measuredHeight / rows
 
-        //create each bitmap piece and add it to the result array
-        var yCoord = 0
+        //create path of each piece and add it to the result array
+        var yCoord = 0 //coordinate "Y" of piece
         for (row in 0 until rows) {
-            var xCoord = 0
+            var xCoord = 0 //coordinate "X" of piece
             for (column in 0 until columns) {
-
-                //draw path
                 val bumpSize = pieceHeight / 4
-                val path = Path()
+                val pathOfPiece = Path()
 
-                path.moveTo(xCoord.toFloat(), yCoord.toFloat())
+                pathOfPiece.moveTo(xCoord.toFloat(), yCoord.toFloat())
 
                 if (row == 0) {
                     //top side piece
-                    path.lineTo(xCoord.toFloat() + pieceWidth, yCoord.toFloat())
+                    topSidePiece(pathOfPiece, xCoord, pieceWidth, yCoord)
                 } else {
                     //top bump
-                    path.lineTo((xCoord + pieceWidth / 3).toFloat(), yCoord.toFloat())
-                    path.cubicTo(
-                        (xCoord + pieceWidth / 6).toFloat(),
-                        (yCoord - bumpSize).toFloat(),
-                        ((xCoord + pieceWidth / 6 * 5)).toFloat(),
-                        (yCoord - bumpSize).toFloat(),
-                        (xCoord + pieceWidth / 3 * 2).toFloat(),
-                        yCoord.toFloat()
-                    )
-                    path.lineTo(xCoord.toFloat() + pieceWidth.toFloat(), yCoord.toFloat())
+                    topBump(pathOfPiece, xCoord, pieceWidth, yCoord, bumpSize)
                 }
                 if (column == columns - 1) {
                     //right side piece
-                    path.lineTo(xCoord.toFloat() + pieceWidth, yCoord.toFloat() + pieceHeight)
+                    rightSidePiece(pathOfPiece, xCoord, pieceWidth, yCoord, pieceHeight)
                 } else {
                     //right bump
-                    path.lineTo(
-                        (xCoord + pieceWidth).toFloat(),
-                        (yCoord + pieceHeight / 3).toFloat()
-                    )
-                    path.cubicTo(
-                        (xCoord + pieceWidth - bumpSize).toFloat(),
-                        (yCoord + pieceHeight / 6).toFloat(),
-                        (xCoord + pieceWidth - bumpSize).toFloat(),
-                        (yCoord + pieceHeight / 6 * 5).toFloat(),
-                        xCoord.toFloat() + pieceWidth.toFloat(),
-                        (yCoord + pieceHeight / 3 * 2).toFloat()
-                    )
-                    path.lineTo(
-                        xCoord.toFloat() + pieceWidth.toFloat(),
-                        yCoord.toFloat() + pieceHeight.toFloat()
-                    )
+                    rightBump(pathOfPiece, xCoord, pieceWidth, yCoord, pieceHeight, bumpSize)
                 }
                 if (row == rows - 1) {
                     //bottom side piece
-                    path.lineTo(
-                        xCoord.toFloat(), yCoord.toFloat() + pieceHeight.toFloat()
-                    )
+                    bottomSidePiece(pathOfPiece, xCoord, yCoord, pieceHeight)
                 } else {
                     //bottom bump
-                    path.lineTo(
-                        (xCoord + pieceWidth / 3 * 2).toFloat(),
-                        yCoord.toFloat() + pieceHeight.toFloat()
-                    )
-                    path.cubicTo(
-                        (xCoord + pieceWidth / 6 * 5).toFloat(),
-                        (yCoord + pieceHeight - bumpSize).toFloat(),
-                        (xCoord + pieceWidth / 6).toFloat(),
-                        (yCoord + pieceHeight - bumpSize).toFloat(),
-                        (xCoord + pieceWidth / 3).toFloat(),
-                        yCoord.toFloat() + pieceHeight.toFloat()
-                    )
-                    path.lineTo(
-                        xCoord.toFloat(),
-                        yCoord.toFloat() + pieceHeight.toFloat()
-                    )
+                    bottomBump(pathOfPiece, xCoord, pieceWidth, yCoord, pieceHeight, bumpSize)
                 }
                 if (column == 0) {
                     //left side piece
-                    path.close()
+                    pathOfPiece.close()
                 } else {
                     //left bump
-                    path.lineTo(
-                        xCoord.toFloat(),
-                        (yCoord + pieceHeight / 3 * 2).toFloat(),
-                    )
-                    path.cubicTo(
-                        (xCoord - bumpSize).toFloat(),
-                        (yCoord + pieceHeight / 6 * 5).toFloat(),
-                        (xCoord - bumpSize).toFloat(),
-                        (yCoord + pieceHeight / 6).toFloat(),
-                        xCoord.toFloat(),
-                        (yCoord + pieceHeight / 3).toFloat()
-                    )
-                    path.close()
+                    leftBump(pathOfPiece, xCoord, yCoord, pieceHeight, bumpSize)
+                    pathOfPiece.close()
                 }
-                path1.addPath(path)
-                path.reset()
+                pathOfView.addPath(pathOfPiece)
+                pathOfPiece.reset()
                 xCoord += pieceWidth
             }
             yCoord += pieceHeight
         }
-        return path1
+        return pathOfView
+    }
+
+    private fun leftBump(path: Path, xCoord: Int, yCoord: Int, pieceHeight: Int, bumpSize: Int) {
+        path.lineTo(
+            xCoord.toFloat(),
+            (yCoord + pieceHeight / 3 * 2).toFloat(),
+        )
+        path.cubicTo(
+            (xCoord - bumpSize).toFloat(),
+            (yCoord + pieceHeight / 6 * 5).toFloat(),
+            (xCoord - bumpSize).toFloat(),
+            (yCoord + pieceHeight / 6).toFloat(),
+            xCoord.toFloat(),
+            (yCoord + pieceHeight / 3).toFloat()
+        )
+    }
+
+    private fun bottomBump(
+        path: Path,
+        xCoord: Int,
+        pieceWidth: Int,
+        yCoord: Int,
+        pieceHeight: Int,
+        bumpSize: Int
+    ) {
+        path.lineTo(
+            (xCoord + pieceWidth / 3 * 2).toFloat(),
+            yCoord.toFloat() + pieceHeight.toFloat()
+        )
+        path.cubicTo(
+            (xCoord + pieceWidth / 6 * 5).toFloat(),
+            (yCoord + pieceHeight - bumpSize).toFloat(),
+            (xCoord + pieceWidth / 6).toFloat(),
+            (yCoord + pieceHeight - bumpSize).toFloat(),
+            (xCoord + pieceWidth / 3).toFloat(),
+            yCoord.toFloat() + pieceHeight.toFloat()
+        )
+        path.lineTo(
+            xCoord.toFloat(),
+            yCoord.toFloat() + pieceHeight.toFloat()
+        )
+    }
+
+    private fun bottomSidePiece(
+        path: Path,
+        xCoord: Int,
+        yCoord: Int,
+        pieceHeight: Int
+    ) {
+        path.lineTo(
+            xCoord.toFloat(), yCoord.toFloat() + pieceHeight.toFloat()
+        )
+    }
+
+    private fun rightBump(
+        path: Path,
+        xCoord: Int,
+        pieceWidth: Int,
+        yCoord: Int,
+        pieceHeight: Int,
+        bumpSize: Int
+    ) {
+        path.lineTo(
+            (xCoord + pieceWidth).toFloat(),
+            (yCoord + pieceHeight / 3).toFloat()
+        )
+        path.cubicTo(
+            (xCoord + pieceWidth - bumpSize).toFloat(),
+            (yCoord + pieceHeight / 6).toFloat(),
+            (xCoord + pieceWidth - bumpSize).toFloat(),
+            (yCoord + pieceHeight / 6 * 5).toFloat(),
+            xCoord.toFloat() + pieceWidth.toFloat(),
+            (yCoord + pieceHeight / 3 * 2).toFloat()
+        )
+        path.lineTo(
+            xCoord.toFloat() + pieceWidth.toFloat(),
+            yCoord.toFloat() + pieceHeight.toFloat()
+        )
+    }
+
+    private fun rightSidePiece(
+        path: Path,
+        xCoord: Int,
+        pieceWidth: Int,
+        yCoord: Int,
+        pieceHeight: Int
+    ) {
+        path.lineTo(xCoord.toFloat() + pieceWidth, yCoord.toFloat() + pieceHeight)
+    }
+
+    private fun topBump(
+        path: Path,
+        xCoord: Int,
+        pieceWidth: Int,
+        yCoord: Int,
+        bumpSize: Int
+    ) {
+        path.lineTo((xCoord + pieceWidth / 3).toFloat(), yCoord.toFloat())
+        path.cubicTo(
+            (xCoord + pieceWidth / 6).toFloat(),
+            (yCoord - bumpSize).toFloat(),
+            ((xCoord + pieceWidth / 6 * 5)).toFloat(),
+            (yCoord - bumpSize).toFloat(),
+            (xCoord + pieceWidth / 3 * 2).toFloat(),
+            yCoord.toFloat()
+        )
+        path.lineTo(xCoord.toFloat() + pieceWidth.toFloat(), yCoord.toFloat())
+    }
+
+    private fun topSidePiece(
+        path: Path,
+        xCoord: Int,
+        pieceWidth: Int,
+        yCoord: Int
+    ) {
+        path.lineTo(xCoord.toFloat() + pieceWidth, yCoord.toFloat())
     }
 
     private fun screenOrientationIsPortrait(): Boolean {
